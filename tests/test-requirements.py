d1b7fe7639c8d23a78d3c97d08928eafb5729520
Rafael Folco <rafaelfolco@gmail.com>
2021-08-27 17:12:15 -0300

    Add a few more tests
    vaalidate requirements with a schema

--- tests/test-requirements.py
+++ tests/test-requirements.py
@@ -7,22 +7,21 @@ import multiplex
 
 class TestRequirements:
 
-    requirements_json = "tests/JSON/requirements-good.json"
-    req_presets_empty = "tests/JSON/requirements-presets-empty-good.json"
+    requirements_json = "requirements-good.json"
+    req_presets_empty = "requirements-presets-empty-good.json"
 
     """Common function to load requirements"""
     @pytest.fixture(scope="function")
     def load_req(self, request):
-        req_json = multiplex.load_json_file(request.param)
-        return(req_json)
+        req_json = multiplex.load_json_file("tests/JSON/"+ request.param)
+        return req_json
 
     """Common function to validate param"""
     @pytest.fixture(scope="function")
     def validate_param(self, request):
         multiplex.log = multiplex.logging.getLogger()
-        multiplex.validation_dict = { "duration": "^[1-9]+[0-9]*$" }
         validated = multiplex.param_validated(next(iter(multiplex.validation_dict)), request.param)
-        return(validated)
+        return validated
 
     """Test if requirements json file is successfully loaded"""
     @pytest.mark.parametrize("load_req", [ requirements_json ], indirect=True)
@@ -47,12 +46,37 @@ class TestRequirements:
         assert multiplex.validation_dict is not {}
         assert load_req['presets'] == {}
 
+    """Test if requirements w/ empty presets validates schema (ok)"""
+    @pytest.mark.parametrize("load_req", [ req_presets_empty ], indirect=True)
+    def test_validate_schema_empty_presets(self, load_req):
+        valid_requirements = multiplex.validate_schema(load_req, "req-schema.json")
+
+        assert valid_requirements is True
+
     """Test invalid vals"""
-    @pytest.mark.parametrize("validate_param", [ "bar", "012", "60s" ], indirect=True)
-    def test_invalid_val(self, validate_param):
+    @pytest.mark.parametrize("validate_param", [ "bar", "012", "60s", "0", "-300", "0.5" ], indirect=True)
+    @pytest.mark.parametrize("val_dict", [ { "duration": "^[1-9]+[0-9]*$" } ])
+    def test_invalid_val(self, validate_param, val_dict):
+        multiplex.validation_dict = val_dict
         assert validate_param is False
 
     """Test valid vals"""
     @pytest.mark.parametrize("validate_param", [ "60", "1", "3600" ], indirect=True)
-    def test_valid_val(self, validate_param):
+    @pytest.mark.parametrize("val_dict", [ { "duration": "^[1-9]+[0-9]*$" } ])
+    def test_valid_val(self, validate_param, val_dict):
+        multiplex.validation_dict = val_dict
+        assert validate_param is True
+
+    """Test invalid vals w/ unit"""
+    @pytest.mark.parametrize("validate_param", [ "0512B", "-64B", "0B", "J" ], indirect=True)
+    @pytest.mark.parametrize("val_dict", [ { "frame-size": "^(([1-9][0-9]*\\.?[0-9]*)|(0?\\.[0-9]+)).*[BKMG]?" } ])
+    def test_invalid_val_unit(self, validate_param, val_dict):
+        multiplex.validation_dict = val_dict
+        assert validate_param is False
+
+    """Test valid vals w/ unit"""
+    @pytest.mark.parametrize("validate_param", [ "64B", "1k", "9216B", "0.1M", "0.001G" ], indirect=True)
+    @pytest.mark.parametrize("val_dict", [ { "duration": "^(([1-9][0-9]*\\.?[0-9]*)|(0?\\.[0-9]+)).*[BKMG]?" } ])
+    def test_valid_val_unit(self, validate_param, val_dict):
+        multiplex.validation_dict = val_dict
         assert validate_param is True
