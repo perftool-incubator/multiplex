f18f9941002f13ebfb2687e939bbbc71d6254d66
Rafael Folco <rafaelfolco@gmail.com>
2021-08-27 11:59:36 -0300

    Load test input json from files
    test json are now loaded from files.

--- tests/test-schema.py
+++ tests/test-schema.py
@@ -12,38 +12,9 @@ class TestSchema:
 
     @pytest.fixture(scope="function")
     def load_json_file(self, request):
-        return multiplex.load_json_file(request.param)
+        return multiplex.load_json_file("tests/JSON/" + request.param)
 
-    @pytest.fixture(scope="function")
-    def load_json_inline(self):
-        input_json="""
-        {
-            "global-options": [
-                {
-                    "name": "common-params",
-                    "params": [
-                        { "arg": "bs", "vals": [ "4k", "8k" ], "role": "client" },
-                        { "arg": "rw", "vals": [ "read", "write" ] }
-                    ]
-                }
-            ],
-            "sets": [
-                {
-                    "include": "common-params",
-                    "params": [
-                        { "arg": "ioengine", "vals": [ "sync" ] }
-                    ]
-                }
-            ]
-        }
-        """
-        try:
-            json_obj = json.loads(input_json)
-        except:
-            return None
-
-        return json_obj
-
-    def test_validate_schema(self, load_json_inline):
-        rt = multiplex.validate_schema(load_json_inline, "schema.json")
+    @pytest.mark.parametrize("load_json_file", [ "validate-schema-good.json" ], indirect=True)
+    def test_validate_schema_good(self, load_json_file):
+        rt = multiplex.validate_schema(load_json_file, "schema.json")
         assert rt == True
